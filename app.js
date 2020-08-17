d2c307b3086152a7250e45a3b6bc1b4999908a4e
Utsav Goswami <utsavg19@googlemail.com>
2020-08-16 23:27:31 -0700

    Cached results

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const port = 3001
+const port = 3001;
+const entries = [];
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -18,7 +19,8 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("home", {
-    homeContent: homeStartingContent
+    homeContent: homeStartingContent,
+    entries: entries
   });
 });
 
@@ -44,13 +46,21 @@ app.post("/compose", (req, res) => {
     postBody: req.body.msg
   }
 
-  console.log(post);
+  entries.push(post);
+
+
+  // Go back to home page after caching the new entry
+  res.redirect("/");
 });
 
+app.get("/posts/:postTitle", (req, res) => {
+  postTitle = req.params.postTitle;
 
-
-
-
-
+  for (entry of entries) {
+    if (entry.title === postTitle) {
+      console.log("Match Found!");
+    }
+  }
+})
 
 app.listen(port, () => console.log("Server started on port", port));
